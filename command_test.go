7e5cd1f257cfb36d3ce68f940dd7b36808e30a2e
QuangTung97 <quangtung29121997@gmail.com>
2021-11-26 11:44:17 +0700

    Fix Test

--- command_test.go
+++ command_test.go
@@ -20,6 +20,41 @@ func TestFindAndGenerate(t *testing.T) {
 	expected := `
 package otelwrap
 
+import (
+	"github.com/QuangTung97/otelwrap/internal/generate/hello"
+	"context"
+	"go.opentelemetry.io/otel/trace"
+	"go.opentelemetry.io/otel/codes"
+)
+
+// SimpleWrapper wraps OpenTelemetry's span
+type SimpleWrapper struct {
+	hello.Simple
+	tracer trace.Tracer
+	prefix string
+}
+
+// NewSimpleWrapper creates a wrapper
+func NewSimpleWrapper(wrapped hello.Simple, tracer trace.Tracer, prefix string) *SimpleWrapper {
+	return &SimpleWrapper{
+		Simple: wrapped,
+		tracer: tracer,
+		prefix: prefix,
+	}
+}
+
+// Handle ...
+func (w *SimpleWrapper) Handle(ctx context.Context, u *hello.User) (err error) {
+	ctx, span := w.tracer.Start(ctx, w.prefix + "Handle")
+	defer span.End()
+
+	err = w.Simple.Handle(ctx, u)
+	if err != nil {
+		span.RecordError(err)
+		span.SetStatus(codes.Error, err.Error())
+	}
+	return err
+}
 `
 	assert.Equal(t, expected, buf.String())
 }
